5bee90ba3f1f082d9809e51cd768978636905d09
shivam singh <shivam.singh.edunext>
2024-05-30 08:37:30 +0530

    admin page

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function toggleSidebar() {
         sidebar.style.left = '0px';
     }
 }
+
+// Function to close the sidebar
+function closeSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.left = '-250px';
+}
+
+// Add event listener to close the sidebar when clicking outside of it
+document.addEventListener('click', function(event) {
+    const sidebar = document.getElementById('sidebar');
+    const burgerIcon = document.querySelector('.burger-icon');
+    const sidebarRect = sidebar.getBoundingClientRect();
+    if (
+        event.clientX < sidebarRect.left ||
+        event.clientX > sidebarRect.right ||
+        event.clientY < sidebarRect.top ||
+        event.clientY > sidebarRect.bottom
+    ) {
+        if (!burgerIcon.contains(event.target)) {
+            closeSidebar();
+        }
+    }
+});
